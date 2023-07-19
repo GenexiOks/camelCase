3c5e87c05d0c65020aa80667a20289b4f3ddad4d
Genexi <Pashaz1994@gmail.com>
2023-07-19 09:06:52 +0300

    home

--- script.js
+++ script.js
@@ -3,4 +3,19 @@ document.body.append(document.createElement('button'));
 const button = document.querySelector('button');
 const text = document.querySelector('textarea');
 
-button.addEventListener('click', function () {});
+button.addEventListener('click', function () {
+  const input = function (value) {
+    const finall = value.toLowerCase().split('\n');
+    for (let n of finall) {
+      n = n.trim().indexof('_');
+      console.log(n);
+    }
+  };
+  input(text.value);
+});
+
+// underscore_case;
+// first_name;
+// next_Variable;
+// Calculate_AGE;
+// arrived_flight;
