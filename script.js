416f0bbd023839f9a866fb0779dd7d6254ff01da
Genexi <Pashaz1994@gmail.com>
2023-07-19 07:29:54 +0300

    home

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+const button = document.querySelector('button');
+const text = document.querySelector('textarea');
+
+button.addEventListener('click', function () {});
